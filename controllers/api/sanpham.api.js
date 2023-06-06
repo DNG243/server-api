d611c77f42c6ac530a95e36ecb9077fc88e5d8b5
DNG243 <duongntph26844@fpt.edu.vn>
2023-06-06 21:07:31 +0700

    api add sp

--- controllers/api/sanpham.api.js
+++ controllers/api/sanpham.api.js
@@ -13,6 +13,29 @@ exports.listSp = async (req, res, next) => {
   } catch (error) {
     return res.status(500).json({ msg: error.message });
   }
-
 };
 
+exports.addSp = async (req, res, next) => {
+  if (req.method == "POST") {
+    let objSP = new md.SpModel();
+    objSP.name = req.body.name;
+    objSP.price = req.body.price;
+    objSP.brand = req.body.brand;
+    objSP.img = req.body.img;
+    objSP.des = req.body.des;
+
+    try {
+      let new_sp = await objSP.save();
+      console.log(new_sp);
+      if (new_sp) {
+        return res
+          .status(200)
+          .json({ data: list, msg: "lay du lieu thanh cong" });
+      } else {
+        return res.status(204).json({ msg: "khong co du lieu" });
+      }
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  }
+};
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,8 @@ var api_u = require('../controllers/api/sanpham.api');
 
 router.get('/sp',api_u.listSp);
 
+router.post('/add',api_u.addSp);
+
 
 
 module.exports = router;
