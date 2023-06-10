21811355d62b6807aa57332fb43b379c79b5d9bc
DNG243 <duongntph26844@fpt.edu.vn>
2023-06-10 22:43:02 +0700

    api dang nhap

--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,16 @@ router.get('/sp',api_sp.listSp);
 
 router.post('/add',api_sp.addSp);
 
-router.get('/login',api_user.Login);
+// router.get('/login',api_user.Login);
+// router.post('/login',api_user.Login);
 
-router.post('/reg',api_user.Reg);
+// router.get('/reg',api_user.Reg);
+// router.post('/reg',api_user.Reg);
 
 router.get('/listUser',api_user.listUser);
+
+router.post('/addUser',api_user.addUser);
+
 
 
 
